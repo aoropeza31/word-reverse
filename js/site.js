b5548d6c5c8561ad0a4aea3d587188bca6936059
Aide Oropeza Aguilar <36251470+aoropeza31@users.noreply.github.com>
2022-05-21 23:05:15 -0400

    completed app

--- js/site.js
+++ js/site.js
@@ -1,4 +1,46 @@
-//call hello world
-function HelloWorld(){
-    alert("hello world");
+//get the string from the page
+
+function getPhrase(){
+    document.getElementById("alert").classList.add("invisible");
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+
+    let userPhrase = document.getElementById("userPhrase").value;
+    let  isPalindrome =  checkIfPalindrome(userPhrase.toLowerCase().replace(/\s+/g, '').replace(regex, ''));
+
+    displayPalindrome(isPalindrome);
+}
+
+function checkIfPalindrome(userPhrase){
+
+    //reverse string
+    let revString = [];
+
+for (let index = userPhrase.length-1; index >= 0; index--) {
+    revString += userPhrase[index];
+    
+}
+
+//check if rev is same as user string
+    if(revString == userPhrase){
+        return true;
+    } else{
+        return false;
+    }
+
+
+}
+
+function displayPalindrome(isPalindrome){
+    if(isPalindrome){
+        let result = "is a palindrome";
+        document.getElementById("msg").innerHTML = `Congrats! Your word/phrase ${result}.`;
+        document.getElementById("alert").classList.remove("invisible");
+
+    }else{
+        let result = "is not a palindrome";
+        document.getElementById("msg").innerHTML = `I'm Sorry! Your word/phrase ${result}.`;
+        document.getElementById("alert").classList.remove("invisible");
+    }
+
 }
